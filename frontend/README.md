cb58f9b70f536ee9f9e729a3e29c7d2a46f89e02
Alexis-Wamster <wamsteralexis@gmail.com>
2025-10-16 15:27:00 +0200

    display work

--- frontend/README.md
+++ frontend/README.md
@@ -3,3 +3,37 @@
 This template should help get you started developing with Vue 3 and TypeScript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
 
 Learn more about the recommended Project Setup and IDE Support in the [Vue Docs TypeScript Guide](https://vuejs.org/guide/typescript/overview.html#project-setup).
+
+
+
+# test the project
+
+To install node.js
+```
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+# in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
+
+# Download and install Node.js:
+nvm install 22
+
+# Verify the Node.js version:
+node -v # Should print "v22.20.0".
+
+# Verify npm version:
+npm -v # Should print "10.9.3".
+```
+
+
+to setup node.js in the front end
+```
+cd ./frontend
+npm install
+```
+
+to execute the project in developer version
+```
+npm run dev
+```
--- frontend/src/core/display/DisplayManager.ts
+++ frontend/src/core/display/DisplayManager.ts
@@ -1,152 +1,200 @@
 import { GameManager } from '../GameManager.ts';
 import { EntityDisplayed } from './EntityDisplayed.ts';
+import { Design } from './Design.ts';
 
-export interface DisplayManagerProps {
-  canvas : HTMLCanvasElement,
-  boxSize : number,
+// interface utilisé pour les entités données par le serveur
+export interface EntityServer {
+  id: number;
+  boxes: [[number,number]];
 }
 
-export class DisplayManager{
+export class DisplayManager {
 
-    private gameManager: GameManager;
+  private gameManager: GameManager;
 
-    private entities : Map<number, EntityDisplayed> = new Map(); // liste des entités présente dans le jeu
-    private canvas! : HTMLCanvasElement; // canvas ou est affiché le jeu
-    private ctx : CanvasRenderingContext2D | null = null; // pinceau permettant d'afficher le jeu
-    private boxSize : number = 0; // taille en pixel des cases
-    private modifiedboxes : Set<string> = new Set(); // liste des cases changé lors d'une animation  (ex: "3,6")
+  private entities: Map<number, EntityDisplayed> = new Map(); // liste des entités présente dans le jeu
+  private canvas!: HTMLCanvasElement | null; // canvas ou est affiché le jeu
+  private ctx: CanvasRenderingContext2D | null = null; // pinceau permettant d'afficher le jeu
+  private boxSize: number = 0; // taille en pixel des cases
+  private modifiedboxes: Set<string> = new Set(); // liste des cases changé lors d'une animation  (ex: "3,6")
+  private inLoop: boolean = false;
 
-    constructor(gameManager: GameManager) {
-        this.gameManager = gameManager
+  constructor(gameManager: GameManager) {
+    this.gameManager = gameManager
+    this.loop = this.loop.bind(this); // bind obligatoire
+  }
+
+  public initialize(canvas: HTMLCanvasElement) {
+    this.setCanvas(canvas);
+    console.log("bonjour alexis");
+  }
+
+  public destroy() {
+    this.canvas = null;
+    this.ctx = null;
+  }
+
+  // ============================ Set ============================ \\
+
+  /**
+   * Change le canvas utilisé pour le jeu et update l'affichage
+   * @param canvas le nouveau canvas
+   */
+  public setCanvas(canvas: HTMLCanvasElement): void {
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext("2d");
+  }
+
+  /**
+   * Change la taille des cases et update l'affichage
+   * @param boxSize nouvelle taille de case en pixel
+   */
+  public setboxSize(boxSize: number): void {
+    this.boxSize = boxSize;
+  }
+
+
+
+  /**
+   * Update les entités affichées
+   * @param enities // objets entité données par le serveur
+   */
+  public setEntities(enities: EntityServer[]): void {
+    enities.forEach(entity => {
+      const entityBoxes = entity.boxes;
+      const entityID = entity.id;
+
+      if (entityID && entityBoxes) {
+        const entityObject = new EntityDisplayed(
+          this,
+          entityBoxes,
+          200,
+          new Design("green"),
+          0
+        );
+        this.setEntity(entityID, entityObject);
+      }
+    });
+  }
+
+  /**
+   * @param id identifié de l'entité à ajouter / ou  à modifier
+   * @param entity objet représentant l'entité
+   */
+  public setEntity(id: number, entity: EntityDisplayed): void {
+    if (this.entities[id]) {
+      this.entities[id].clear();
     }
-    
-    public initialize({ canvas, boxSize } : DisplayManagerProps) {
-        this.boxSize = boxSize;
-        this.setCanvas(canvas);
+    this.entities.set(id,entity);
+  }
+
+  // ============================ Get ============================ \\
+
+  /**
+   * @returns renvoie le pinceau permettant d'afficher des éléments sur la grille
+   */
+  public getCtx(): CanvasRenderingContext2D {
+    if (!this.ctx) {
+      throw new Error("Contexte 2D non initialisé. Assurez-vous d'appeler setCanvas avant.");
     }
-    
-    public destroy() {
+    return this.ctx
+  }
 
+  /**
+   * @returns Renvoie la taille en pixel d'une case de jeu
+   */
+  public getBoxSize(): number {
+    return this.boxSize;
+  }
+
+  // ============================ Methodes publiques ============================ \\
+
+  /**
+   * Vérifie si une case à été effacé pendant l'animation
+   * @param coordinate Coordonnée de la case à vérifier
+   * @returns si la case à été effacé ou non
+   */
+  public existeModifiedBox(coordinate: number[]): boolean {
+    return this.modifiedboxes.has(coordinate.join("_"));
+  }
+
+  public startLoop() : void{
+    if (!this.inLoop){
+      this.inLoop = true;
+      this.loop();
     }
+  }
 
-    // ============================ Set ============================ \\
+  public stopLoop() : void{
+    this.inLoop = false;
+  }
 
-    /**
-     * Change le canvas utilisé pour le jeu et update l'affichage
-     * @param canvas le nouveau canvas
-     */
-    public setCanvas(canvas : HTMLCanvasElement) : void{
-        this.canvas = canvas;
-        this.ctx = this.canvas.getContext("2d");
-        this.show();
+  // ============================ Méthodes d'affichage ============================ \\
+
+  /**
+   * Efface une case de la grille de jeu
+   */
+  public clearBox(coordinate: number[]): void {
+    if (!this.existeModifiedBox(coordinate)) {
+      this.getCtx().clearRect(
+        coordinate[0] * this.getBoxSize(),
+        coordinate[1] * this.getBoxSize(),
+        this.getBoxSize(),
+        this.getBoxSize()
+      );
+      this.addModifiedbox(coordinate);
     }
+  }
 
-    /**
-     * Change la taille des cases et update l'affichage
-     * @param boxSize nouvelle taille de case en pixel
-     */
-    public setboxSize(boxSize: number) : void{
-        this.boxSize = boxSize;
-        this.show();
+  /**
+   * Update entièrement la grille de jeu
+   */
+  public show(): void {
+    if (this.canvas !== null) {
+      this.getCtx().clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.entities.forEach((entity : EntityDisplayed, id : number) => {
+        entity.setFullAnimation(true);
+        entity.animate(Date.now());
+      });
     }
+  }
 
-    /**
-     * @param id identifié de l'entité à ajouter / ou  à modifier
-     * @param entity objet représentant l'entité
-     */
-    public setEntity(id : number, entity : EntityDisplayed) : void{
-        if (this.entities[id]){
-            this.entities[id].clear();
-        }
-        this.entities[id] = entity;
+  /**
+   * Update les partie qui ont changé sur la grille de jeu
+   */
+  public animate(): void {
+    this.clearModifiedboxes();
+    console.log(this.entities.size);
+    this.entities.forEach((entity : EntityDisplayed) => {
+      entity.animate(Date.now());
+      console.log("yo",entity);
+    });
+    this.clearModifiedboxes();
+  };
+
+  // ============================ Methodes privées de case modifié ============================ \\
+
+  /**
+   * Signal qu'une case à été effacé
+   * @param coordinate coordonnée de la case effacée
+   */
+  private addModifiedbox(coordinate: number[]): void {
+    this.modifiedboxes.add(coordinate.join(","));
+  }
+
+  /**
+   * reset l'attribut modifiedboxes, pour indiquer qu'ancune case n'a été effacé
+   */
+  private clearModifiedboxes(): void {
+    this.modifiedboxes = new Set();
+  }
+  /**
+   * La boucle de jeu
+   */
+  private loop() {
+    this.animate();
+    if (this.inLoop){
+      requestAnimationFrame(this.loop);
     }
-
-    // ============================ Get ============================ \\
-
-    /**
-     * @returns renvoie le pinceau permettant d'afficher des éléments sur la grille
-     */
-    public getCtx() : CanvasRenderingContext2D {
-        if (!this.ctx) {
-            throw new Error("Contexte 2D non initialisé. Assurez-vous d'appeler setCanvas avant.");
-        }
-        return this.ctx
-    }
-    
-    /**
-     * @returns Renvoie la taille en pixel d'une case de jeu
-     */
-    public getBoxSize() : number{
-        return this.boxSize;
-    }
-
-    // ============================ Methodes publiques de case modifié ============================ \\
-    
-    /**
-     * Vérifie si une case à été effacé pendant l'animation
-     * @param coordinate Coordonnée de la case à vérifier
-     * @returns si la case à été effacé ou non
-     */
-    public existeModifiedBox(coordinate : number[]): boolean{
-        return this.modifiedboxes.has(coordinate.join("_"));
-    }
-
-    // ============================ Méthodes d'affichage ============================ \\
-
-    /**
-     * Efface une case de la grille de jeu
-     */
-    public clearBox(coordinate : number[]) : void{
-        if (!this.existeModifiedBox(coordinate)){
-            this.getCtx().clearRect(
-                coordinate[0]*this.getBoxSize(),
-                coordinate[1]*this.getBoxSize(),
-                this.getBoxSize(),
-                this.getBoxSize()
-            );
-            this.addModifiedbox(coordinate);
-        }
-    }
-
-    /**
-     * Update entièrement la grille de jeu
-     */
-    public show() : void{
-        this.getCtx().clearRect(0, 0, this.canvas.width, this.canvas.height);
-        for (const entity of this.entities.values()) {
-            entity.setFullAnimation(true);
-            entity.animate();
-        };
-    }
-
-    /**
-     * Update les partie qui ont changé sur la grille de jeu
-     */
-    public animate() : void{
-        this.clearModifiedboxes();
-        for (const entity of this.entities.values()) {
-            entity.clearChange();
-        };
-        for (const entity of this.entities.values()) {
-            entity.animate(Date.now());
-        };
-        this.clearModifiedboxes();
-    };
-
-    // ============================ Methodes privées de case modifié ============================ \\
-
-    /**
-     * Signal qu'une case à été effacé
-     * @param coordinate coordonnée de la case effacée
-     */
-    private addModifiedbox(coordinate : number[]) : void{
-        this.modifiedboxes.add(coordinate.join(","));
-    }
-
-    /**
-     * reset l'attribut modifiedboxes, pour indiquer qu'ancune case n'a été effacé
-     */
-    private clearModifiedboxes() : void{
-        this.modifiedboxes = new Set();
-    }
+  }
 }
